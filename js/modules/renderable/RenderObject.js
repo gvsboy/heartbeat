3ed9958f333da704394ac1081ce7d882534905a6
James Kranz <jameskranz@gmail.com>
2013-01-26 17:13:41 -0800

    Add velocity and acceleration to RenderObject.

--- js/modules/renderable/RenderObject.js
+++ js/modules/renderable/RenderObject.js
@@ -4,11 +4,33 @@
 define('RenderObject', ['renderable/Renderable'], function(r) {
 
   exports.RenderObject = r.Renderable.extend(function(base) {
-    // Private
 
-    // Public
-    function init (g) {
+    function getVel() {
+      return this.vel;
+    }
+    
+    function setVel(val) {
+      this.vel = val;
+    }
+
+    function getAcc() {
+      return this.acc;
+    }
+
+    function update() {
+      // Accelerate
+      this.vel.x += this.acc.x;
+      this.vel.y += this.acc.y;
+
+      // Move
+      this.move(vel.x, vel.y);
+    }
+
+    function init(g, vel, acc) {
       base.init.call(this, g);
+
+      this.vel = vel || {x: 0, y: 0};
+      this.acc = acc || {x: 0, y: 0};
     }
 
     return {
