385fac41fb41247f85397865eb9dc9832ed12423
James Kranz <jameskranz@gmail.com>
2013-01-26 17:38:42 -0800

    Formatting changes in some files, and got a little demo of object velocity and acceleration.

--- js/modules/main.js
+++ js/modules/main.js
@@ -1,4 +1,4 @@
-define("main", ["renderable/Renderable","renderable/RenderObject"], function(Renderable, RenderObject) {
+define("main", ["renderable/Renderable","renderable/RenderObject", "renderable/Player"], function(Renderable, RenderObject, Player) {
   var stage = new createjs.Stage(document.getElementById('canvas'));
 
   // Just test code
@@ -18,15 +18,20 @@ define("main", ["renderable/Renderable","renderable/RenderObject"], function(Ren
 
   var b = new RenderObject.RenderObject(graphics2);
 
+  var c = new Player.Player({x:1,y:1}, {x:0.1, y:0.1});
   a.setPosition({x:20,y:20});
 
   stage.addChild(a.symbol);
   stage.addChild(b.symbol);
+  stage.addChild(c.symbol);
   
   // End test code
 
 
   // Have stage listen to the ticker.
-  createjs.Ticker.addListener(stage);
+  createjs.Ticker.addListener(function() {
+    c.update();
+    stage.update();
+  });
 
 });
